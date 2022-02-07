64f3d43cc26d59048c8f8acc466afae7822de2c9
suminerProxy <97101851+suminerProxy@users.noreply.github.com>
2022-02-07 20:37:40 +0800

    4.9修复
    4.9修复

--- start.sh
+++ start.sh
@@ -4,13 +4,19 @@ touch start.log
 chmod 777 start.log
  
 while [ 1 ] ; do
+    if [ $(ps -ef|grep minerproxy|grep -v grep|wc -l) -eq 0 ] ; then
+        sleep 1;
+        echo "[`date +%F\ %T`] minerproxy is offline, try to restart..." >> start.log
+        ./minerproxy &
+    else
+        echo "[`date +%F\ %T`] minerproxy is online..." >> start.log
+    fi
     if [ $(ps -ef|grep tproxy |grep -v grep|wc -l) -eq 0 ] ; then
         sleep 1;
-        echo "[`date +%F\ %T`] tproxyis offline, try to restart..." >> start.log
+        echo "[`date +%F\ %T`] tproxy is offline, try to restart..." >> start.log
         ./tproxy > tproxy.log 2>&1 &
     else
         echo "[`date +%F\ %T`] tproxyis online..." >> start.log
     fi
- 
     sleep 10
 done
